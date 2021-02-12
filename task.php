d1cfbd702519c7fd34ac9c43b3c2829fd352e2b7
yutakanashiro <kanashiro713@gmail.com>
2021-02-13 04:24:50 +0900

    php基礎課題1~8

--- task.php
+++ task.php
@@ -6,6 +6,9 @@ $names = ["田中", "佐藤", "佐々木", "高橋"];
 
   # 以下に回答を記載
 
+$name[] = "斎藤";
+print_r($name);
+
 echo PHP_EOL;
 
 print("#####q2#####".PHP_EOL);
@@ -14,6 +17,9 @@ $array2 = ["bird", "bat", "tiger"];
 
   # 以下に回答を記載
 
+$array = array_merge($array1,$array2);
+print_r($array);
+
 echo PHP_EOL;
 
 print("#####q3#####".PHP_EOL);
@@ -21,12 +27,29 @@ $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9];
 
   # 以下に回答を記載
 
+$counter = 0;
+
+foreach($numbers as $number){
+
+   if($number == 3){
+      $counter++;
+      echo $counter."回";
+   }
+
+}
+
+
 echo PHP_EOL;
 
 print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
   # 以下に回答を記載
+
+$sports = array_diff($sports,[null]);
+
+print_r($sports);
+  
 
 echo PHP_EOL;
 
@@ -36,12 +59,32 @@ $array2 = [1, 5, 8, 10];
 
   # 以下に回答を記載
 
+if(empty($array1)){
+   echo "true";
+}else {
+   echo "false";
+}
+
+echo "<br>";
+
+if(empty($array2)){
+   echo "true";
+}else{
+   echo "false";
+}
+
 echo PHP_EOL;
 
 print("#####q6#####".PHP_EOL);
 $numbers1 = [1, 2, 3, 4, 5];
 
   # 以下に回答を記載
+
+foreach($numbers1 as $number){
+   $numbers2[] = $number * 10;
+}
+
+print_r($numbers2);
 
 echo PHP_EOL;
 
@@ -50,12 +93,18 @@ $array = ["1", "2", "3", "4", "5"];
 
   # 以下に回答を記載
 
+$array = array_map(intval,$array);
+print_r($array);
+
 echo PHP_EOL;
 
 print("#####q8#####".PHP_EOL);
 $programming_languages = ["php","ruby","python","javascript"];
 
   # 以下に回答を記載
+
+$programming_languages = array_map(ucfirst,$programming_languages);
+$upper_case_programming_languages = array_map(strtoupper,$programming_languages);
 
   # 以下は変更しないで下さい
 print_r($programming_languages);
